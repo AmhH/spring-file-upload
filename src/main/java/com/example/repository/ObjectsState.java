11e0d1f14d3346772d95d8a185c457afc57d3cba
Amh <abaams78@gmail.com>
2020-12-16 11:23:07 -0600

    hibernate flush

--- src/main/java/com/example/repository/ObjectsState.java
+++ src/main/java/com/example/repository/ObjectsState.java
@@ -4,8 +4,12 @@ import com.example.model.File;
 import java.util.List;
 import javax.persistence.Query;
 import org.hibernate.LockMode;
+import org.hibernate.ReplicationMode;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.engine.spi.SessionFactoryDelegatingImpl;
 
 public class ObjectsState {
   
@@ -28,7 +32,42 @@ public class ObjectsState {
     q.setFirstResult(12);
     q.setMaxResults(10);
 
+    //for filtering
     Restrictions.eq("f", save);
+    sess.saveOrUpdate(save);
   }
+
+  public void replicationDataStores(){
+    //retrieve a cat from one database
+    SessionFactory factory1 = null;
+    SessionFactory factory2 = null;
+    Session session1 = factory1.openSession();
+    Transaction tx1 = session1.beginTransaction();
+    File cat = session1.get(File.class, "fileId");
+    tx1.commit();
+    session1.close();
+
+//reconcile with a second database
+    Session session2 = factory2.openSession();
+    Transaction tx2 = session2.beginTransaction();
+    session2.replicate(cat, ReplicationMode.LATEST_VERSION);
+    tx2.commit();
+    session2.close();
+  }
+  /**
+   * Flush
+   *  flush, occurs by default at the following points:
+   *
+   * before some query executions
+   * from org.hibernate.Transaction.commit()
+   * from Session.flush()
+   * The SQL statements are issued in the following order:
+   * all entity insertions in the same order the corresponding objects were saved using Session.save()
+   * all entity updates
+   * all collection deletions
+   * all collection element deletions, updates and insertions
+   * all collection insertions
+   * all entity deletions in the same order the corresponding objects were deleted using Session.delete()
+   */
 
 }
